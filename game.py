86b81572510e8b26647295a9a17a5323f0c9f664
chipzymango <chipzymango2@gmail.com>
2022-03-20 19:20:44 +0100

    Enhanced images and added food

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 pygame.init()
 
@@ -12,6 +13,11 @@ WINDOW = pygame.display.set_mode((WINDOW_X, WINDOW_Y))
 pygame.display.set_caption("Sidewinder")
 
 clock = pygame.time.Clock()
+
+
+DEFAULT_FONT = pygame.font.Font('font/dpcomic.ttf', 30)
+game_over_text = DEFAULT_FONT.render("This is the default font !", 1, (255,0,0))
+
 
 cell_list = []
 object_list = []
@@ -29,33 +35,44 @@ y_axis = 25
 
 bg_color = (255, 225, 200)
 
-grid_border_color = (225,200,175)#bg_color
+grid_border_color = bg_color#(225,200,175)#bg_color
 
 # player start position
 target_cell_x, target_cell_y = 200, 200
 
-player_head_image = pygame.image.load('images/head.png')
+show_rects = False
+
+player_head_image = pygame.image.load('images/head.png').convert()
+player_head_image.set_colorkey((255,255,255))
 player_head_image = pygame.transform.scale(player_head_image, (player_head_image.get_width()*2*cell_size , player_head_image.get_height()*2*cell_size))
 
-player_body_image = pygame.image.load('images/body.png')
+player_body_image = pygame.image.load('images/body.png').convert()
+player_body_image.set_colorkey((255,255,255))
 player_body_image = pygame.transform.scale(player_body_image, (player_body_image.get_width()*2*cell_size , player_body_image.get_height()*2*cell_size))
 
-player_tail_image = pygame.image.load('images/tail.png')
+player_tail_image = pygame.image.load('images/tail.png').convert()
+player_tail_image.set_colorkey((255,255,255))
 player_tail_image = pygame.transform.scale(player_tail_image, (player_tail_image.get_width()*2*cell_size , player_tail_image.get_height()*2*cell_size))
 
-player_joint_image = pygame.image.load('images/joint.png')
+player_joint_image = pygame.image.load('images/joint.png').convert()
+player_joint_image.set_colorkey((255,255,255))
 player_joint_image = pygame.transform.scale(player_joint_image, (player_joint_image.get_width()*2*cell_size, player_joint_image.get_height()*2*cell_size))
 
 class Object():
-	def __init__(self, start_x, start_y):
+	def __init__(self, start_x, start_y, object_type='food'):
+		self._id = len(object_list)
+		self.object_type = object_type
 		self.rect = pygame.Rect(start_x, start_y, cell_width, cell_width)
 		self.width = cell_width
-		self.image = pygame.image.load("images/food.png").convert()
+		self.image = pygame.image.load("images/"+object_type+".png").convert()
+		self.image.set_colorkey((255,255,255))
 		self.image = pygame.transform.scale(self.image, (self.width, self.width))
+		self.active = True
 		object_list.append(self)
 
 	def update(self):
-		WINDOW.blit(self.image, (self.rect.x, self.rect.y))
+		if self.active:
+			WINDOW.blit(self.image, (self.rect.x, self.rect.y))
 
 class BodyPart():
 	def __init__(self, start_x, start_y):
@@ -73,6 +90,7 @@ class BodyPart():
 			self.direction =  body_part_list[self._id-1].direction
 
 	def update(self):
+		# decide which image to show according to list
 		if self == body_part_list[0]:
 			self.image = player_head_image
 		elif self == body_part_list[-1]:
@@ -93,6 +111,7 @@ class BodyPart():
 				if self.rect.y + cell_width >= WINDOW_Y:
 					self.rect.y = 0
 				else:
+
 					self.rect.y += cell_width
 				self.image = pygame.transform.rotate(self.image, 270)
 
@@ -126,7 +145,8 @@ class BodyPart():
 		self.coordinates = (self.rect.x, self.rect.y)
 
 	def draw(self):
-		pygame.draw.rect(WINDOW, (255,50,50), self.rect)
+		if show_rects:
+			pygame.draw.rect(WINDOW, (255,50,50), self.rect)
 		WINDOW.blit(self.image, (self.rect.x, self.rect.y))
 
 def start_game(): # reset game values
@@ -135,6 +155,8 @@ def start_game(): # reset game values
 
 	body_part_list.clear()
 	object_list.clear()
+
+	spawn_object()
 
 	start_head_body_part = BodyPart(target_cell_x, target_cell_y)
 	start_middle_body_part = BodyPart(start_head_body_part.rect.x-cell_width, start_head_body_part.rect.y)
@@ -146,15 +168,23 @@ def game_over():
 
 	game_over_loop = True
 
-	screen_layer = pygame.Surface((WINDOW_X, WINDOW_Y)).convert_alpha()
+	screen_layer = pygame.Surface((WINDOW_X, WINDOW_Y)).convert()
+	
 	while game_over_loop:
-		screen_layer.fill((0,0,0,0.5))
+		screen_layer.fill((0,0,0))
+		screen_layer.set_alpha(.5)
+		screen_layer.blit(game_over_text, (50, 50))
+		WINDOW.blit(screen_layer, (0,0))
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				exit()
+
 			if event.type == pygame.KEYDOWN:
 				game_over_loop = False
+
+		pygame.display.update()
 
 	start_game()
 
@@ -164,16 +194,41 @@ def draw_grid_layout():
 		# draw borders
 		pygame.draw.rect(WINDOW, grid_border_color, each_cell, 1, 0, 1)
 
-def spawn_object():
+def spawn_object(object_type='food'):
+	target_cell_occupied = False
 	rx = random.randint(1, x_axis-1)
 	ry = random.randint(1, y_axis-1)
+
+	# if an object is already occupying the cell, get new coordinates
+	for objects in object_list:
+		if rx == objects.rect.x and ry == objects.rect.y:
+			rx = random.randint(1, x_axis-1)
+			ry = random.randint(1, y_axis-1)
+
+	for bodies in body_part_list:
+		if rx == bodies.rect.x and ry == bodies.rect.y:
+			rx = random.randint(1, x_axis-1)
+			ry = random.randint(1, y_axis-1)
+
 	rx = rx * cell_width
 	ry = ry * cell_width
-	new_object = Object(rx, ry)
-	print("Spawned new object at: " + str(rx) + ", " + str(ry))
+
+	for bodypart in body_part_list:
+		if rx == bodypart.rect.x and ry == bodypart.rect. y:
+			target_cell_occupied = True
+			print("Cell occupied by body part !")
+
+	for objects in object_list:
+		if rx == objects.rect.x and ry == objects.rect.y:
+			target_cell_occupied = True
+			print("Cell occupied by obstacle !")
+
+	if not target_cell_occupied:
+		new_object = Object(rx, ry, object_type)
+		print("Spawned new object at: " + str(rx) + ", " + str(ry) + " with id: " + str(new_object._id) )
 
 def player_grow():
-	# spawn a new body part behind the last body part
+	# spawn a new body part behind the last body part	
 	new_rect = BodyPart(body_part_list[-1].rect.x, body_part_list[-1].rect.y) 
 
 for each_object in object_list:
@@ -195,7 +250,8 @@ for each_cell in cell_list:
 # timers to control various events
 display_update_timer = pygame.time.get_ticks()
 obstacle_spawn_timer = pygame.time.get_ticks()
-
+direction_change_cooldown_timer = pygame.time.get_ticks()
+direction_change_cooldown = 66
 display_update_delay = 100
 
 # starter body rect
@@ -203,10 +259,12 @@ start_head_body_part = BodyPart(target_cell_x, target_cell_y)
 start_middle_body_part = BodyPart(start_head_body_part.rect.x-cell_width, start_head_body_part.rect.y)
 start_tail_body_part = BodyPart(start_middle_body_part.rect.x-cell_width, start_middle_body_part.rect.y)
 
+spawn_object()
+
 FPS = 60
 running = True
 while running:
-	clock.tick(FPS) # set 
+	clock.tick(FPS) # set fixed framerate
 
 	WINDOW.fill(bg_color) # draw background color
 
@@ -220,31 +278,40 @@ while running:
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_RIGHT:
 				if body_part_list[0].direction == 'up' or body_part_list[0].direction == 'down':
-					body_part_list[0].direction = 'right'
+					if pygame.time.get_ticks() - direction_change_cooldown_timer > direction_change_cooldown:
+						body_part_list[0].direction = 'right'
+						direction_change_cooldown_timer = pygame.time.get_ticks()
+
 
 			if event.key == pygame.K_DOWN:
 				if body_part_list[0].direction == 'left' or body_part_list[0].direction == 'right':
-					body_part_list[0].direction = 'down'
+					if pygame.time.get_ticks() - direction_change_cooldown_timer > direction_change_cooldown:
+						body_part_list[0].direction = 'down'
+						direction_change_cooldown_timer = pygame.time.get_ticks()
 
 			if event.key == pygame.K_LEFT:
 				if body_part_list[0].direction == 'up' or body_part_list[0].direction == 'down':
-					body_part_list[0].direction = 'left'
+					if pygame.time.get_ticks() - direction_change_cooldown_timer > direction_change_cooldown:
+						body_part_list[0].direction = 'left'
+						direction_change_cooldown_timer = pygame.time.get_ticks()						
 
 			if event.key == pygame.K_UP:
 				if body_part_list[0].direction == 'left' or body_part_list[0].direction == 'right':
-					body_part_list[0].direction = 'up'
+					if pygame.time.get_ticks() - direction_change_cooldown_timer > direction_change_cooldown:
+						body_part_list[0].direction = 'up'
+						direction_change_cooldown_timer = pygame.time.get_ticks()
 
 			if event.key == pygame.K_SPACE:
 				player_grow()
 
-	# call update method in every existing object
-	for each_object in object_list:
-		each_object.update()
-
 	# spawn a new object every third second
-	if pygame.time.get_ticks() - obstacle_spawn_timer > 3000:
-		obstacle_spawn_timer = pygame.time.get_ticks() 
-		spawn_object()
+	if pygame.time.get_ticks() - obstacle_spawn_timer > 5000:
+		obstacle_spawn_timer = pygame.time.get_ticks()
+		rng = random.randint(0, 10)
+		if rng == 7:
+			spawn_object('food')
+		else: 
+			spawn_object('obstacle')
 
 	if pygame.time.get_ticks() - display_update_timer > display_update_delay:
 		display_update_timer = pygame.time.get_ticks()
@@ -298,6 +365,10 @@ while running:
 	for bodypart in body_part_list:
 		bodypart.draw()
 
+	# call update method in every existing object
+	for each_object in object_list:
+		each_object.update()
+
 	# assign each rect an id and check if a body
 	# rect has collided with the player head rect
 	for body_id in range(len(body_part_list)):
@@ -310,11 +381,18 @@ while running:
 				game_over()
 				break # break so the loop does not continue after the game reset
 
-
+	# check if an object has collided with player
 	for objects in object_list:
-		if body_part_list[0].rect.colliderect(objects):
-			print("You died at: " + str(body_part_list[0].rect.x) + ", " + str(body_part_list[0].rect.y))
-			game_over()
-			break
+		if body_part_list[0].rect.colliderect(objects.rect) and objects.active == True:
+			if objects.object_type == 'food':
+				player_grow()
+
+				spawn_object()
+			elif objects.object_type == 'obstacle':
+				print("player head has collided with: obstacle id {0} !".format(objects._id))
+				game_over()
+				break
+
+			objects.active = False
 
 	pygame.display.update()
